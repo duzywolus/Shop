e1941465cba9a32baa7516d8231faa46992ad508
duzywolus <duzywolus@interia.pl>
2023-03-14 20:42:53 +0100

    Add user listing page

--- OnlineShopWebParent/OnlineShopBackEnd/src/test/java/com/onlineshop/admin/user/UserRepositoryTest.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/test/java/com/onlineshop/admin/user/UserRepositoryTest.java
@@ -10,12 +10,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 
 import com.onlineshop.common.entity.Role;
 import com.onlineshop.common.entity.User;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTest {
@@ -123,4 +126,17 @@ public class UserRepositoryTest {
 		userRepository.updateEnabledStatus(id, true);
 	}
 	
+	@Test
+	public void testListFirstPage( ) {
+		int pageNumber = 0;
+		int pageSize = 4; 
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = userRepository.findAll(pageable);
+		
+		List<User> listUsers = page.getContent();
+		
+		listUsers.forEach(user -> System.out.println(user));
+		
+		assertThat(listUsers.size()).isEqualTo(pageSize);
+	}
 }
