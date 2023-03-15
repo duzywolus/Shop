b6b9686b3859ea90b0f6153d569e080502d51007
duzywolus <duzywolus@interia.pl>
2023-03-15 19:33:14 +0100

    Add user sorting

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/user/UserController.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/user/UserController.java
@@ -28,13 +28,17 @@ public class UserController {
 
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model);
+		return listByPage(1, model, "firstName", "asc");
 	}
 	
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-		Page<User> page = userService.listByPage(pageNum);
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir) {
+		Page<User> page = userService.listByPage(pageNum, sortField, sortDir);
 		List<User> listUsers = page.getContent();
+		
+		System.out.println("Sort field: " + sortField);
+		System.out.println("Sort direction: " + sortDir);
 		
 		long startCount = (pageNum -1) * UserService.USERS_PER_PAGE + 1;
 		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
@@ -42,12 +46,18 @@ public class UserController {
 			endCount = page.getTotalElements();
 		}
 		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
 		model.addAttribute("endCount", endCount);
 		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		
 		return "users";
 	}
 
