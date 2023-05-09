ea6b941f6473b540d54343828f69339c690c128d
duzywolus <duzywolus@interia.pl>
2023-05-09 20:32:04 +0200

    Login function implementation

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/security/WebSecurityConfig.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/security/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.onlineshop.admin.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,17 +20,43 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
 	@Bean
+	public UserDetailsService userDetailsService() {
+		return new OnlineShopDetailsService();
+	}
+	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	 @Bean
-	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	        http
-	            .authorizeHttpRequests((authz) -> authz
-	                .anyRequest().permitAll()
-	            );
-	        return http.build();
-	    }
-
+	
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
 	}
+	
+	
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login")
+		.usernameParameter("email")
+		.permitAll();
+
+		return http.build();
+	}
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+
+}
