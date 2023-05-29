83997314b2d33dc98773f1a29996c61bbe56eeae
duzywolus <duzywolus@interia.pl>
2023-05-29 22:05:37 +0200

    User authorization implementation

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/security/WebSecurityConfig.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/security/WebSecurityConfig.java
@@ -25,35 +25,41 @@ public class WebSecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService());
 		authProvider.setPasswordEncoder(passwordEncoder());
-		
+
 		return authProvider;
 	}
-	
-	
+
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin().loginPage("/login")
-		.usernameParameter("email")
-		.permitAll()
-		.and().logout().permitAll()
-		.and().rememberMe().key("AbcDefgHijKlmnOpqrs_1234567890")
-		.tokenValiditySeconds(7 * 24 * 60 * 60);
+		http.authorizeHttpRequests().requestMatchers("/users/**").hasAuthority("Admin")
+				.requestMatchers("/categories/**").hasAnyAuthority("Admin", "Editor")
+				.requestMatchers("/brands/**").hasAnyAuthority("Admin", "Editor")
+				.requestMatchers("/products/**").hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+				.requestMatchers("/questions/**").hasAnyAuthority("Admin", "Assistant")
+				.requestMatchers("/reviews/**").hasAnyAuthority("Admin", "Assistant")
+				.requestMatchers("/customers/**").hasAnyAuthority("Admin", "Salesperson")
+				.requestMatchers("/orders/**").hasAnyAuthority("Admin", "Salesperson", "Shipper")
+				.requestMatchers("/shipping/**").hasAnyAuthority("Admin", "Salesperson")
+				.requestMatchers("/report/**").hasAnyAuthority("Admin", "Salesperson")
+				.requestMatchers("/articles/**").hasAnyAuthority("Admin", "Editor")
+				.requestMatchers("/menus/**").hasAnyAuthority("Admin", "Editor")
+				.requestMatchers("/settings/**").hasAuthority("Admin")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login")
+				.usernameParameter("email").permitAll().and().logout().permitAll().and().rememberMe()
+				.key("AbcDefgHijKlmnOpqrs_1234567890").tokenValiditySeconds(7 * 24 * 60 * 60);
 
 		return http.build();
 	}
-	
+
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
