447472df45fc0441b88039a5af1868cd4b81c019
duzywolus <duzywolus@interia.pl>
2023-07-19 22:02:48 +0200

    Category sorting implementation

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/category/CategoryController.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/category/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -26,10 +27,17 @@ public class CategoryController {
 	private CategoryService categoryService;
 	
 	@GetMapping("/categories")
-	public String listAll(Model model) {
-		List<Category> listCategories = categoryService.listAll();
-		model.addAttribute("listCategories", listCategories);
+	public String listAll(@Param("sortDir") String sortDir,Model model) {
+		if (sortDir == null || sortDir.isEmpty()) {
+			sortDir = "asc";
+		}
 		
+		List<Category> listCategories = categoryService.listAll(sortDir);
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("reverseSortDir", reverseSortDir);
 		return "categories/categories";
 	}
 	
