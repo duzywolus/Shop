a77b79b69046c2c27d4d81a5f77b29b0fbbb346d
duzywolus <duzywolus@interia.pl>
2023-07-31 19:23:11 +0200

    Category pagination implementation. Refactor pagination code for User

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/category/CategoryController.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/category/CategoryController.java
@@ -27,14 +27,27 @@ public class CategoryController {
 	private CategoryService categoryService;
 	
 	@GetMapping("/categories")
-	public String listAll(@Param("sortDir") String sortDir,Model model) {
+	public String listFirstPage(@Param("sortDir") String sortDir,Model model) {
+		return listByPage(1, sortDir, model);
+	}
+	
+	@GetMapping("/categories/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, 
+			@Param("sortDir") String sortDir, Model model) {
 		if (sortDir == null || sortDir.isEmpty()) {
 			sortDir = "asc";
 		}
 		
-		List<Category> listCategories = categoryService.listAll(sortDir);
+		CategoryPageInfo categoryPageInfo = new CategoryPageInfo();
+		List<Category> listCategories = categoryService.listByPage(categoryPageInfo, pageNum, sortDir);
 		
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		model.addAttribute("totalPages", categoryPageInfo.getTotalPages());
+		model.addAttribute("totalItems", categoryPageInfo.getTotalPages());
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("sortField", "name");
+		model.addAttribute("sortDir", sortDir);
 		
 		model.addAttribute("listCategories", listCategories);
 		model.addAttribute("reverseSortDir", reverseSortDir);
