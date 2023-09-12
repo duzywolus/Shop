6de47694f133147c47be827a32eaadeb3e3fbf7d
duzywolus <duzywolus@interia.pl>
2023-09-12 22:20:53 +0200

    Create tests for brand repository

--- OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/brand/BrandRepository.java
+++ OnlineShopWebParent/OnlineShopBackEnd/src/main/java/com/onlineshop/admin/brand/BrandRepository.java
@@ -1,9 +1,10 @@
 package com.onlineshop.admin.brand;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.onlineshop.common.entity.Brand;
 
-public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>, CrudRepository<Brand, Integer> {
 
 }
